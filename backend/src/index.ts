b87ee8d734b36714cbbcb1e734484bf971629d5f
habanero-love <5kontir0ea@gmail.com>
2026-03-16 07:29:59 +0900

    backend/src/index.ts、portを修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running: http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
