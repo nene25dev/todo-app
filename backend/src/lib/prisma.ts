5d01ffaf24f519a7efa3e48d53e61efe6d28855c
habanero-love <5kontir0ea@gmail.com>
2026-03-17 08:19:04 +0900

    prisma.ts、SQLite用の記述を修正

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,16 +1,19 @@
 import "dotenv/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL!;
 
 if (!databaseUrl) {
   throw new Error("DATABASE_URL is not set");
 }
 
-// Prisma用 SQLite 接続ドライバ
-const adapter = new PrismaBetterSqlite3({
-  url: databaseUrl,
+// Prisma用 postgresql 接続ドライバ
+const pool = new pg.Pool({
+  connectionString: databaseUrl,
 });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
